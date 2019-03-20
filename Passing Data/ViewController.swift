757d7a8f8eddf612ae2a505f01540158056f2793
Dom <2montd10@solent.ac.uk>
2019-03-20 19:23:30 +0000

    Initial Commit

---
+++ Passing Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Passing Data
+//
+//  Created by Dom Montalto on 20/03/2019.
+//  Copyright © 2019 Dom Montalto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
